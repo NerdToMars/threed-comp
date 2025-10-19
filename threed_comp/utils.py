409d2e6d4fcc29a1e35fbc225619ed3a55c8a5b2
PH <ctian.huang@gmail.com>
2025-10-19 15:48:08 +0800

    add resource

--- threed_comp/utils.py
+++ threed_comp/utils.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import os
 
+
 def get_modelnet10_metadata():
-    # read the metadata_modelnet10.csv file 
+    # read the metadata_modelnet10.csv file
     meta_data_path = os.path.join(os.path.dirname(__file__), "metadata_modelnet10.csv")
     metadata = pd.read_csv(meta_data_path)
     return metadata
