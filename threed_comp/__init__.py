65cd2b172b24e685572ae2eb3860e3d72fd1032b
PH <ctian.huang@gmail.com>
2025-10-29 00:26:49 +0800

    add author info

---
+++ threed_comp/__init__.py
@@ -0,0 +1,5 @@
+def print_authors_info():
+    print("Authors:")
+    print("HUANG chongtian, NTU, Singapore")
+    print("ZHOU hanzhang, NTU, Singapore")
+    print("Description: This project is a assignment project for course MA6514")
